471a9cab672c35933154a6b144e47c66e861b800
yukihirop <te108186@gmail.com>
2020-12-24 15:28:42 +0900

    Add Formula: ultraman

---
+++ Formula/ultraman.rb
@@ -0,0 +1,22 @@
+# https://qiita.com/dalance/items/b07bee6cadfd4dd19756
+class Ultraman < Formula
+  version '0.1.0'
+  desc "Manage Procfile-based applications. (Rust Foreman)"
+  homepage "https://github.com/yukihirop/ultraman"
+
+  dependes_on "rust" => :build
+  
+  if OS.mac?
+    url "https://github.com/yukihirop/ultraman/releases/download/v0.1.0/ultraman-v0.1.0-x86_64-mac.zip"
+    sha256 '806358564886618e29767a4fab1b1e1456ff1fe5bc496ae9908968b68c5fee64'
+  end
+
+  if OS.linux?
+    url "https://github.com/yukihirop/ultraman/releases/download/v0.1.0/ultraman-v0.1.0-x86_64-linux.zip"
+    sha256 '9123ae1fd343c1d7a1641e6c5fd8b513d901b50894a37d5977c9f11f794d2931'
+  end
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
+  end
+end
