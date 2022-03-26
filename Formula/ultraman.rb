ad6a36ccaa0425fceac6c64537a3d8e02eeff4ba
yukihirop <te108186@gmail.com>
2022-03-26 15:32:15 +0900

    feat: 🎸 bump ultraman to v0.1.3

--- Formula/ultraman.rb
+++ Formula/ultraman.rb
@@ -1,19 +1,19 @@
 # https://qiita.com/dalance/items/b07bee6cadfd4dd19756
 class Ultraman < Formula
-  version '0.1.2'
+  version '0.1.3'
   desc "Manage Procfile-based applications. (Rust Foreman)"
   homepage "https://github.com/yukihirop/ultraman"
 
   depends_on "rust" => :build
   
   if OS.mac?
-    url "https://github.com/yukihirop/ultraman/releases/download/v0.1.2/ultraman-v0.1.2-x86_64-mac.zip"
-    sha256 'e10b79fea9575bedc804895902231d85c0ecb0b8d2fb11c4951306cf0a4405f5'
+    url "https://github.com/yukihirop/ultraman/releases/download/v0.1.3/ultraman-v0.1.3-x86_64-mac.zip"
+    sha256 '0ee6d6f8c72de6449dae5b0602ff1b8ae7e9cb6023382e1307cdec0fc16e828b'
   end
 
   if OS.linux?
-    url "https://github.com/yukihirop/ultraman/releases/download/v0.1.2/ultraman-v0.1.2-x86_64-linux.zip"
-    sha256 '746894c846018893197da782184d061e91cddfe92571ed9342592ef47f374188'
+    url "https://github.com/yukihirop/ultraman/releases/download/v0.1.3/ultraman-v0.1.3-x86_64-linux.zip"
+    sha256 '933d5fa22a5b3d90632d2c674005c1555e759e8e0fd7f587220c3f9e8c749c25'
   end
 
   head 'https://github.com/yukihirop/ultraman.git'
